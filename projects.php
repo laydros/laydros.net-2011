28510c8b04765ac32602758a9db6243a9255081f
laydros <laydros@gmail.com>
2011-02-13 21:43:59 -0500

    updated projects.php to use jekyll

--- projects.php
+++ projects.php
@@ -1,38 +1,14 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-       "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta name="description" content="Stuff">
-		<meta name="keywords" content="text, text files, sync, syncing, writing, notes, todo, to-do, todo list, productivity" />
-		<meta name="viewport" content="width=device-width" />
-		<title>laydros.org</title>
-		<link rel="icon" href="/favicon.ico" TYPE="image/x-icon">
-		<style type="text/css" media="all"> 
-			@import "mainstyle.css";
-		</style>
-			<script type="text/javascript">	
-			</script>
-			<script type="text/javascript" src="jquery.js"></script>
-
-	</head>
-	
-	<body>	
-			
-		<?php
-		include("header.inc");
-		?>
-
+---
+layout: default
+title: projects
+---
 		<div id="sometext">
-			Page in progress. Most of the code is up at github
 		<a href="https://github.com/laydros">My page at Github</a><br />
-
+    <br />
+    <ul>
+        <li><a href="https://github.com/laydros/laydros.net">Code for this site</a></li>
+        <li><a href="https://github.com/laydros/reNoted">reNoted - note taking application for windows influenced by notational velocity</a></ul>
+        <li><a href="https://github.com/laydros/wattago.com">wattago.com - in progress twitter web application.</a></li>
+    </ul>
 		<br /><br />
 		</div>
-
-		<?php
-			include("footer.inc")
-		?>
-
-	</body>
-</html>
