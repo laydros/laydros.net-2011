703de39cef7c62cc59206d553ac28ace683eaff9
laydros <laydros@gmail.com>
2011-02-13 22:09:58 -0500

    updated resume and links on index

--- index.php
+++ index.php
@@ -5,11 +5,13 @@ title: home
 		<div id="sometext">
 		<a href="/about.php">about</a><br />
 		<a href="/projects.php">projects</a><br />
-		<a href="/hamilton_resume.pdf">resume</a><br />
+    <a href="/bloglist.php">blog</a><br />
+		<a href="/media/hamilton_resume.pdf">resume</a><br />
 		<a href="gallery.php">photography</a><br />
 		<a href="http://flickr.com/photos/jwhamilton">flickr</a><br />
 		<a href="http://www.smilinglikeareptile.com">smiling like a reptile</a><br />
 		<a href="http://twitter.com/laydros">twitter</a><br />
+    <a href="http://identi.ca/laydros">identi.ca</a><br />
 		<br /><br />
 		</div>
 
